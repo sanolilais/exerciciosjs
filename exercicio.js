d175e3b454016c01cced00acc4cc256ca68396fe
Lais <sanolilais@gmail.com>
2022-01-28 01:16:06 -0300

    initial commit

---
+++ exercicio.js
@@ -0,0 +1,9 @@
+const numinteiro = 540; 
+
+const num1 = ('5')
+const num2 = ('4')
+const num3 = ('0')
+
+
+
+console.log(num1 + num2 + num3); 
---
+++ exercicio2.js
@@ -0,0 +1,9 @@
+const  num , lado1, lado2, lado3, perimetro; 
+
+lado1 = (prompt('Digite a medida do primeiro lado'))
+lado2 = (prompt('Digite a medida do segundo lado'))
+lado3 = (prompt('Digite a medida do terceiro lado'))
+
+perimetro = lado1 + lado2 + lado3
+
+alert('o perimetro é ', perimetro)
